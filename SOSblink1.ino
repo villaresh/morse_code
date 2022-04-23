0af3f6201f2d6186763ab2ec88ea6eafceb67740
villaresh <100607769+villaresh@users.noreply.github.com>
2022-04-23 19:04:56 +0200

    Create SOSblink1.ino

---
+++ SOSblink1.ino
@@ -0,0 +1,51 @@
+/* Created on 2022
+ *  Last modification: 21st April 2022
+ *  by Villares
+ */
+const int ledPin = 5;           // LED connected to pin number 5
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  //S -> ... (3 dots)
+  digitalWrite(ledPin, HIGH);    // turn the LED on (HIGH is the voltage level)
+  delay(500);       // wait for 0.5 seconds
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);      // wait for 0.5 seconds
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+  //O -> --- (3 dashes)
+  digitalWrite(ledPin, HIGH);
+  delay(1500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+  digitalWrite(ledPin, HIGH);
+  delay(1500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+  digitalWrite(ledPin, HIGH);
+  delay(1500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+  //S -> ... (3 dots)
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
+  }
