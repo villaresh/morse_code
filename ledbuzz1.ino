5ef32a94b1ffb01489caa2fad58599c6f6e3669a
Villares <100607769+villaresh@users.noreply.github.com>
2022-04-25 01:45:15 +0200

    Update ledbuzz1.ino

--- ledbuzz1.ino
+++ ledbuzz1.ino
@@ -3,8 +3,8 @@
  *  This code permits to execute the SOS both on the LED and Buzzer at the same time
  */
 
-const int ledPin = 5;     //D1
-const int pinBuzzer = 4;  //D2
+const int ledPin = 5;            // LED connected to pin number 5 (D1)
+const int pinBuzzer = 4;         // Buzzer connected to pin number 4 (D2)
 
 void setup() {
 pinMode(pinBuzzer,OUTPUT);
@@ -12,12 +12,12 @@ pinMode(ledPin, OUTPUT);
 }
 
 void SOS(int n) {
-  tone(pinBuzzer, 440);
-  digitalWrite(ledPin, HIGH); 
-   delay(n);
-  noTone(pinBuzzer);
-  digitalWrite(ledPin, LOW); 
-   delay(500);
+  tone(pinBuzzer, 440);          // turn the Buzzer on at a frequency of 440 Hz
+  digitalWrite(ledPin, HIGH);    // turn the LED on (HIGH is the voltage level)
+   delay(n);                     // wait for n seconds
+  noTone(pinBuzzer);             // turn the Buzzer off
+  digitalWrite(ledPin, LOW);     // turn the LED off by making the voltage LOW
+   delay(500);                   // wait for 0.5 seconds
   tone(pinBuzzer, 440);
   digitalWrite(ledPin, HIGH); 
    delay(n);
