0e5e4378653767339e91df663d6501a631e9f5b2
Villares <100607769+villaresh@users.noreply.github.com>
2022-05-01 18:19:50 +0200

    Update ldr2.ino

--- ldr2.ino
+++ ldr2.ino
@@ -1,8 +1,8 @@
 /* Created on 2022
-   * Last modification: 1st May 2022
-   * This code permits to make a LDR blink (depends on the light) with an external LED
-   */
-const int ledPin = D1;    // builtin LED
+ * Last modification: 1st May 2022
+ * This code permits to make a LDR blink (depends on the light) with an external LED
+ */
+const int ledPin = D1;    // external LED pin
 const int ldrPin = A0;    // LDR connected to pin A0
 
 void setup() {    // the setup routine runs once when you press reset
@@ -15,14 +15,14 @@ void loop() {     // the loop routine runs over and over again forever
 
 int ldrStatus = analogRead(ldrPin);   // Reads the value from the LDR analog pin
 
-  if (ldrStatus > 900) {
+  if (ldrStatus > 900) {    // if statement checks for a condition and executes the proceeding statement
     digitalWrite(ledPin, HIGH);   // The pin has been defined as an OUTPUT, so voltage will be set to the corresponding value (5V or 3.3V)
     Serial.print(ldrStatus);    // prints the data to the serial port
     Serial.println("LDR is DARK, LED is ON");   // This text is seen when opening the Serial Monitor, it's only to make sure that the LDR is working-> this line can be deleted
   }
 
-  else {
-    digitalWrite(ledPin, LOW);
+  else {    // it is executed in any situation which do not meet the if condition
+    digitalWrite(ledPin, LOW);      // turn the LED off by making the voltage LOW
     Serial.println("LED is OFF");   // Prints data to the serial port as human-readable ASCII text (this line can also be deleted)
   }
   delay(100);   // LDR status is checked every 100ms
